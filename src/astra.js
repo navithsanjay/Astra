05e8f8f743772273c580bcdee7ce52d34070e10a
nandhakrishnanp <nandhakrishnandev@gmail.com>
2024-03-10 12:08:40 +0530

    new [page

--- src/astra.js
+++ src/astra.js
@@ -1,50 +1,134 @@
 import React from "react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init({ once: true });
+const Astra = () => {
+  return (
+    <div className="container">
+      <div className="header text-center p-2">
+        <h1 className="rob">Team Astra</h1>{" "}
+        <p className="cl">Unleash the Power : Where Speed Meets Precision</p>
+      </div>
 
+      <div className="about-us">
+        <h3 className="pop cl">About Team Astra</h3>
 
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis cum
+          ex fugit beatae ipsam! Iste vero velit voluptatem facere saepe hic,
+          tenetur odio, vitae temporibus et sequi architecto soluta doloremque?
+          Nemo ratione culpa, odio ab omnis itaque possimus dolores nobis, eum
+          esse ipsam obcaecati blanditiis id tenetur repellat numquam accusamus
+          reiciendis tempora. Dolorem ex natus, dignissimos eaque suscipit quos
+          ab. Nam, voluptatibus ipsam sunt quidem libero quae mollitia quis
+          laboriosam quisquam vero eligendi at, beatae impedit ea, consectetur
+          a. Fuga odio, atque asperiores dolore sunt maiores laudantium facilis
+          eum qui? Unde corporis nihil dolorum vero consequatur. Id nam pariatur
+          reiciendis voluptatum. Labore ex libero corporis saepe tempora fuga
+          blanditiis, perspiciatis nemo totam, consequatur repellat praesentium
+          necessitatibus ducimus mollitia dignissimos quis! Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Expedita sunt nobis excepturi
+          facilis dicta repellat alias animi sed, dolorum optio autem,
+          temporibus similique in perferendis accusamus, praesentium dolorem
+          molestiae nemo. Voluptas hic impedit aliquid, nobis sed blanditiis
+          numquam possimus nostrum ipsam eius itaque enim pariatur assumenda
+          ipsa quia ducimus! Repudiandae, illo assumenda! Consectetur voluptates
+          quis voluptate vitae maiores perspiciatis ipsa. Mollitia sed itaque
+          labore similique optio quia voluptatibus, earum ea fugit suscipit est
+          commodi, voluptas ad cum atque totam impedit officiis blanditiis in
+          reiciendis alias dolor? Reiciendis officiis nam commodi. Molestias
+          minus quia nemo repellat quisquam omnis dolor voluptatum, cumque
+          suscipit praesentium ducimus placeat quo odit porro illum consequuntur
+          unde excepturi beatae sequi expedita! Nostrum magnam cupiditate
+          sapiente excepturi quasi? Delectus et nemo asperiores alias quisquam!
+          Odio voluptatum eligendi, cupiditate quo expedita quod. Obcaecati
+          vitae laudantium voluptates explicabo harum itaque, et voluptas
+          suscipit quos voluptatum distinctio possimus soluta officia. Corporis?
+          Non eius ad, a impedit praesentium, ut reiciendis possimus, eaque
+          debitis aspernatur ex suscipit repellendus perspiciatis dolores iure
+          placeat. A minus unde sed maxime veritatis quisquam eius possimus
+          store, magni nihil officiis repellendus voluptas distinctio laboriosam
+          impedit placeat totam.
+        </p>
+      </div>
 
-const Astra =()=>{
+      <div className="row m-5">
+        <h3 className="text-center cl m-3">Our Journey</h3>
+        <div
+          className="col-md-6"
+          data-aos="fade-right"
+          data-aos-duration="1000"
+        >
+          <img
+            className="img-fluid rounded size"
+            src="https://ideogram.ai/api/images/direct/uIMKRRKyRveVyO_SJkr-uQ.png"
+            alt=""
+          />
+        </div>
 
-  return  (
-    <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        What is Astra?
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        <div className="col-md-6" data-aos="fade-left" data-aos-duration="1000">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            accusamus incidunt aliquid porro tenetur id, laborum corporis. Quasi
+            quia, modi non assumenda inventore, sit numquam facilis maiores eos
+            quaerat consequatur! Maiores incidunt in, explicabo ipsam iste
+            commodi voluptatibus, unde accusamus saepe culpa quaerat? Amet
+            tempora blanditiis veniam pariatur quos vitae voluptatem nesciunt,
+            unde beatae quam fugiat neque cupiditate consectetur quo! Dicta
+            eligendi officiis nulla numquam perferendis quisquam alias animi,
+            cum deserunt nobis officia quas doloribus inventore laboriosam
+            asperiores aliquam perspiciatis, omnis quia illum itaque temporibus.
+            Autem consequuntur esse ut rem! Eveniet omnis excepturi, velit totam
+            iusto repellendus nihil rerum nisi facilis laudantium aperiam saepe.
+            Consequuntur nemo aut blanditiis maxime rem. Quis ratione veniam
+            libero ducimus, at possimus necessitatibus ab corporis. Quibusdam
+            quis hic pariatur nihil dicta labore deleniti architecto
+            necessitatibus veniam! Quia repellat necessitatibus, consequuntur
+          </p>
+        </div>
+      </div>
+
+      <div className="row m-5 ">
+        <div
+          className="col-md-6  order-md-1 order-2"
+          data-aos="fade-right"
+          data-aos-duration="1000"
+        >
+          <p>
+            second Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ipsam accusamus incidunt aliquid porro tenetur id, laborum corporis.
+            Quasi quia, modi non assumenda inventore, sit numquam facilis
+            maiores eos quaerat consequatur! Maiores incidunt in, explicabo
+            ipsam iste commodi voluptatibus, unde accusamus saepe culpa quaerat?
+            Amet tempora blanditiis veniam pariatur quos vitae voluptatem
+            nesciunt, unde beatae quam fugiat neque cupiditate consectetur quo!
+            Dicta eligendi officiis nulla numquam perferendis quisquam alias
+            animi, cum deserunt nobis officia quas doloribus inventore
+            laboriosam asperiores aliquam perspiciatis, omnis quia illum itaque
+            temporibus. Autem consequuntur esse ut rem! Eveniet omnis excepturi,
+            velit totam iusto repellendus nihil rerum nisi facilis laudantium
+            aperiam saepe. Consequuntur nemo aut blanditiis maxime rem. Quis
+            ratione veniam libero ducimus, at possimus necessitatibus ab
+            corporis. Quibusdam quis hic pariatur nihil dicta labore deleniti
+            architecto necessitatibus veniam! Quia repellat necessitatibus,
+            consequuntur
+          </p>
+        </div>
+        <div
+          className="col-md-6  order-md-2 order-1 "
+          data-aos="fade-left"
+          data-aos-duration="1000"
+        >
+          <img
+            className="img-fluid rounded"
+            src="https://ideogram.ai/api/images/direct/JmfuSSLHREGIOmiDETjTxA.jpg"
+            alt=""
+          />
+        </div>
       </div>
     </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
 export default Astra;
